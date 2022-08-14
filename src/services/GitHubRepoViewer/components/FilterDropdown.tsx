bd8b5c39995b74eb20a00585d1bfd4839939a9e5
Maks Ufa <Maksymilian.Ufa@i4-insight.com>
2022-08-14 20:20:00 +0200

    added debounce for search input

--- src/services/GitHubRepoViewer/components/FilterDropdown.tsx
+++ src/services/GitHubRepoViewer/components/FilterDropdown.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Input } from "antd";
+import debounce from "lodash/debounce";
+import { DEBOUNCE_THRESHOLD } from "../utils/constants";
+
+String!;
 
 interface IFilteredDropdown {
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 }
 
-// TODO: make better search UX with debounce
+// make better search UX with debounce
 function FilterDropdown({ setSearchQuery }: IFilteredDropdown) {
+  const debouncedSearch = debounce((value: string) => {
+    setSearchQuery(value);
+  }, DEBOUNCE_THRESHOLD);
+
   return (
     <Input
       allowClear
       onChange={(e: React.FormEvent<HTMLInputElement>) =>
-        setSearchQuery(e.currentTarget.value)
+        debouncedSearch(e.currentTarget.value)
       }
     />
   );
--- src/services/GitHubRepoViewer/utils/constants.ts
+++ src/services/GitHubRepoViewer/utils/constants.ts
@@ -1,3 +1,4 @@
 export const PAGE_SIZE = 10;
+export const DEBOUNCE_THRESHOLD = 300;
 /* normally this color shouldn't be passed in-line and retrieved from well-defined app pallette */
 export const STARS_COLOR = "#FFDF00";
