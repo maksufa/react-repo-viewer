2090e4d6189748ac0ec0b0efad006eb81c5bfa1d
Maks Ufa <Maksymilian.Ufa@i4-insight.com>
2022-08-14 20:48:06 +0200

    fix: hiden and deleted private creds

--- src/gql/ApolloClientSetup.ts
+++ src/gql/ApolloClientSetup.ts
@@ -5,9 +5,7 @@ const GQLClient = new ApolloClient({
   uri: "https://api.github.com/graphql",
   headers: {
     // put below your Github Personal Access Token to enable GQL queries - https://docs.github.com/en/graphql/guides/forming-calls-with-graphql
-
-    // TODO: delete this PAT from Github once repo becomes public
-    authorization: "Bearer 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", // <Your Personal Access Token>
+    authorization: "Bearer <Your Personal Access Token>",
   },
   // prevent by refetching repos when traversing through those which have been already fetched
   cache: new InMemoryCache(),
